67d7139074b3eef011a04a7b5f7cfb75506380b3
Your Name <you@example.com>
2023-04-08 15:37:26 -0600

    jeje

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,14 +10,15 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Professional" | "Casual" | "Funny";
+export type VibeType = "1st Grade" | "2nd Grade" | "3rd Grade" | "4th Grade" | "5th Grade" | "6th Grade";
 
 interface DropDownProps {
   vibe: VibeType;
   setVibe: (vibe: VibeType) => void;
 }
 
-let vibes: VibeType[] = ["Professional", "Casual", "Funny"];
+let vibes: VibeType[] = ["1st Grade", "2nd Grade", "3rd Grade", "4th Grade", "5th Grade", "6th Grade"];
+
 
 export default function DropDown({ vibe, setVibe }: DropDownProps) {
   return (
