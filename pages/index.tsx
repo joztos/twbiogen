5e00fd7a88f09b08b27b9bb50a3901ccd4d24659
Your Name <you@example.com>
2023-04-08 16:14:29 -0600

    4

--- pages/index.tsx
+++ pages/index.tsx
@@ -163,7 +163,7 @@ const Home: NextPage = () => {
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                 {generatedBios
                   .substring(generatedBios.indexOf("1") + 3)
-                  .split("2.")
+                  .split("4.")
                   .map((generatedBio) => {
                     return (
                       <div
