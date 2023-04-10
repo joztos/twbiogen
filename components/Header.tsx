8c3dce58e677763d289398206c89779ff5d87e51
Your Name <you@example.com>
2023-04-10 17:56:41 -0600

    yeyeye

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,28 +4,22 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
-      <Link href="/" className="flex space-x-3">
+      <div className="flex space-x-3">
         <Image
-          alt="EMDI Logo"
-          src="https://emdischool.edu.ec/port/images/logo%20emdi%20azul.png?crc=397001260"
+          alt="Custom Logo"
+          src="https://drive.google.com/uc?id=1dSfqMquSlUpfgqUB2CjdMi2Fv10TYOwx"
           className="sm:w-12 sm:h-12 w-8 h-8 object-contain"
           width={48}
           height={48}
         />
-      </Link>
-      <a
-        href="https://samasat.com/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          alt="Vercel Icon"
-          src="/vercelLogo.png"
-          className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
-          width={32}
-          height={28}
-        />
-      </a>
+      </div>
+      <Image
+        alt="Vercel Icon"
+        src="/vercelLogo.png"
+        className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
+        width={32}
+        height={28}
+      />
     </header>
   );
 }
